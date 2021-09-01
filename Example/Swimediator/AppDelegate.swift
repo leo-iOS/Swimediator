a8bc932bb46120501cd8205035fd70a13debb504
李傲 <liao@qiyuhz.com>
2021-09-01 17:05:58 +0800

    Add: 增加三个modules

--- Example/Swimediator/AppDelegate.swift
+++ Example/Swimediator/AppDelegate.swift
@@ -11,6 +11,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        MediatorManager.addModules([Service1Module(), Service2Module(), Service3Module()])
         return true
     }
 
